509f48dc4fe430f97689c38586ce8cb06cb99151
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-06-12 08:08:58 +0200

    wip

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/api/friends/3', (req, res) => res.json({ id: '3', name: 'Gabriel', age: 42 }));
-app.get('/api/friends', (req, res) => res.json(
-  [
-    { id: '3', name: 'Gabriel', age: 42 },
-    { id: '12', name: 'Josemaria', age: 26 },
-    { id: '8', name: 'Anna', age: 24 },
-  ],
-));
+const friends = [
+  { id: '1', name: 'Anna', age: 24 },
+  { id: '2', name: 'Josemaria', age: 26 },
+  { id: '3', name: 'Gabriel', age: 41 },
+];
+
+app.get('/api/friends', (req, res) => res.json(friends));
+app.get('/api/friends/1', (req, res) => res.json(friends[0]));
+app.get('/api/friends/2', (req, res) => res.json(friends[1]));
+app.get('/api/friends/3', (req, res) => res.json(friends[2]));
 
 app.listen(port, () => console.log(`Friends server listening on port ${port}!`));
