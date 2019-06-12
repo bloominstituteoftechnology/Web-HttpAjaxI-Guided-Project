f484b1f054d6696a4a5fe3c5653d93062fb52829
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-06-12 08:44:23 +0200

    wip

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -7,12 +7,6 @@ import styled from 'styled-components';
 const StyledContainer = styled.div`
   padding: 20px;
   height: 100%;
-
-  nav {
-    padding: 0 20px;
-    display: flex;
-    justify-content: space-between;
-  }
 `;
 
 export default class Container extends React.Component {
